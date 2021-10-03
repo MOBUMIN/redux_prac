6b8bed66acb87bad32667cbae0770b83d0bdd112
MOBUMIN <kiju23@naver.com>
2021-10-03 13:07:38 +0900

    react-redux 적용

--- front/src/containers/AddNumber.jsx
+++ front/src/containers/AddNumber.jsx
@@ -1,11 +1,22 @@
 import AddNumber from "../components/AddNumber";
-import React, { Component } from 'react';
-import store from "../store";
+import { connect } from 'react-redux';
 
-export default class extends Component {
-	render() {
-		return <AddNumber onClick={function(size){
-			store.dispatch({type:'INCREMENT', size:size})
-		}.bind(this)}></AddNumber>
+function mapReduxDispatchToReactProps(dispatch){
+	// redux의 dispatch를 react 컴포넌트의 props로 연결시키는
+	return {
+		onClick: function(size){
+			dispatch({type:'INCREMENT', size:size})
+		}
 	}
 }
+
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+// mapReduxStateToReactProps, mapReduxDispatchToReactProps
+
+// export default class extends Component {
+// 	render() {
+// 		return <AddNumber onClick={function(size){
+// 			store.dispatch({type:'INCREMENT', size:size})
+// 		}.bind(this)}></AddNumber>
+// 	}
+// }
--- front/src/containers/DisplayNumber.jsx
+++ front/src/containers/DisplayNumber.jsx
@@ -1,16 +1,23 @@
 import DisplayNumber from "../components/DisplayNumber";
-import React, { Component } from 'react';
-import store from "../store";
+import { connect } from 'react-redux';
 
-export default class extends Component{
-	state = {number:store.getState().number}
-	constructor(props){
-		super(props);
-		store.subscribe(function(){
-			this.setState({number:store.getState().number});
-		}.bind(this))
-	}
-	render(){
-		return <DisplayNumber number={this.state.number}></DisplayNumber>
+function mapReduxStateToReactProps(state){
+	// 구독 + 전달될 값 이름 정하기
+	return {
+		number: state.number
 	}
 }
+export default connect(mapReduxStateToReactProps)(DisplayNumber);
+
+// export default class extends Component{
+// 	state = {number:store.getState().number}
+// 	constructor(props){
+// 		super(props);
+// 		store.subscribe(function(){
+// 			this.setState({number:store.getState().number});
+// 		}.bind(this))
+// 	}
+// 	render(){
+// 		return <DisplayNumber number={this.state.number}></DisplayNumber>
+// 	}
+// }
